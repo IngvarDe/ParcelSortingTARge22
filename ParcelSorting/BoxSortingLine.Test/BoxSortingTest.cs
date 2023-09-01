52dd739f70144f1b4ea98eb09ccc30b48fb7d1d6
Ingvar Derešivski <inde999@gmail.com>
2023-09-01 10:37:15 +0300

    Fix

--- ParcelSorting/BoxSortingLine.Test/BoxSortingTest.cs
+++ ParcelSorting/BoxSortingLine.Test/BoxSortingTest.cs
@@ -11,33 +11,7 @@ namespace BoxSortingLine.Test
     {
         public void When_ParcelHasNewDimensionsAndSortingLineHasNewDimensions_ThenParcelCanNotFitSortingLine()
         {
-            var boxSizes = new List<BoxSize>()
-            {
-                new BoxSize
-                {
-                    Length = 70,
-                    Width = 50,
-                    SortingLineParams = new List<SortingLineParam>()
-                    {
-                        new SortingLineParam
-                        {
-                            LineWidth = 50,
-                        },
-                        new SortingLineParam
-                        {
-                            LineWidth = 50,
-                        },
-                        new SortingLineParam
-                        {
-                            LineWidth = 100,
-                        },
-                        new SortingLineParam
-                        {
-                            LineWidth = 100,
-                        }
-                    }
-                }
-            };
+            
         }
     }
 }
